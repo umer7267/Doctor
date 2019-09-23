8e1987043346221425130bc897a839bec62c4854
Umer <Umer7267@gmail.com>
2019-09-23 16:05:58 +0500

    Implementing Navigation Drawer

--- app/src/main/java/com/healthtracker/doctor/doctor/DoctorMainActivity.java
+++ app/src/main/java/com/healthtracker/doctor/doctor/DoctorMainActivity.java
@@ -1,13 +1,21 @@
 package com.healthtracker.doctor.doctor;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.healthtracker.doctor.LoginActivity;
 import com.healthtracker.doctor.R;
@@ -19,10 +27,15 @@ public class DoctorMainActivity extends AppCompatActivity {
 
     Button logout;
 
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
+    private NavigationView nav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_main);
+        navBar();
 
         logout = findViewById(R.id.logout);
 
@@ -36,5 +49,74 @@ public class DoctorMainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void navBar() {
+        drawerLayout = findViewById(R.id.drawer);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        nav = findViewById(R.id.nav);
+        nav.inflateMenu(R.menu.nav_menu_doctor);
+
+        View headerView = nav.getHeaderView(0);
+
+        CircleImageView profileImage = headerView.findViewById(R.id.profile_image);
+        TextView name = headerView.findViewById(R.id.name);
+        TextView email = headerView.findViewById(R.id.email);
+
+        name.setText("Muhammad Umer");
+        email.setText("Ayan90899@gmail.com");
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                switch (id){
+                    case R.id.profile:
+                        Intent intentProfile = new Intent(context, DoctorProfileActivity.class);
+                        startActivity(intentProfile);
+                        break;
+
+                    case R.id.notification:
+                        Intent intentNotification = new Intent(context, DoctorNotifications.class);
+                        startActivity(intentNotification);
+                        break;
+
+                    case R.id.logout:
+                        FirebaseAuth.getInstance().signOut();
+                        Intent intent = new Intent(context, LoginActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        finish();
+                        startActivity(intent);
+                        break;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(drawerToggle.onOptionsItemSelected(item))
+            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/healthtracker/doctor/doctor/DoctorProfileActivity.java
@@ -0,0 +1,16 @@
+package com.healthtracker.doctor.doctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.healthtracker.doctor.R;
+
+public class DoctorProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_profile);
+    }
+}
